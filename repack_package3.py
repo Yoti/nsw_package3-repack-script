820703dfc6a703864db064b1139749dad5433af8
Yoti <realyoti@gmail.com>
2025-07-19 01:04:28 +0300

    v0.2

--- repack_package3.py
+++ repack_package3.py
@@ -12,7 +12,7 @@ def bs_int32(src, start, reverse=0):
         return(int.from_bytes(ret, 'big'))
 
 def main():
-    print('PK31 BPatcher v0.1 by Yoti')
+    print('PK31 BPatcher v0.2 by Yoti')
 
     in_files = [os.path.join('atmo', 'package3'), os.path.join('kefir', 'package3')]
     for i in range(len(in_files)):
@@ -29,17 +29,18 @@ def main():
         if not a_head == b'PK31':  # проверка на соответствие формата в заголовке
             sys.exit(f'{in_files[0]} is broken')
 
+        # статичное значение смещения взято из упаковщика
+        a_atmo_vernum = a_data[0x38:0x3C]
+        # смещения взяты из рассчёта, что boot упакован 3 с конца
         a_boot_offset = bs_int32(a_data, 0x1C0)
         a_boot_f_size = bs_int32(a_data, 0x1C4)
         a_boot_offse2 = bs_int32(a_data, 0x4F8)
         a_boot_f_siz2 = bs_int32(a_data, 0x4FC)
+        # необязательные проверки для дополнительной перестраховки
         if not a_boot_f_size == a_boot_f_siz2:
             sys.exit(f'{in_files[0]} is broken')
         if not a_boot_offset == a_boot_offse2 + 0x100000:
             sys.exit(f'{in_files[0]} is broken')
-
-        a_boot_f_data = a_data[a_boot_offset:a_boot_offset+a_boot_f_size]
-        a_boot_sha256 = a_data[0x500:0x520]
 
     with open(in_files[1], 'rb') as k:  # 'kefir -> package3'
         k_data = bytearray(k.read())
@@ -49,15 +50,24 @@ def main():
         if not k_head == b'PK31':  # проверка на соответствие формата в заголовке
             sys.exit(f'{in_files[1]} is broken')
 
+        # статичное значение смещения взято из упаковщика
+        k_atmo_vernum = k_data[0x38:0x3C]
+        # смещения взяты из рассчёта, что boot упакован 3 с конца
         k_boot_offset = bs_int32(k_data, 0x1C0)
         k_boot_f_size = bs_int32(k_data, 0x1C4)
         k_boot_offse2 = bs_int32(k_data, 0x4F8)
         k_boot_f_siz2 = bs_int32(k_data, 0x4FC)
+        # необязательные проверки для дополнительной перестраховки
         if not k_boot_f_size == k_boot_f_siz2:
             sys.exit(f'{in_files[1]} is broken')
         if not k_boot_offset == k_boot_offse2 + 0x100000:
             sys.exit(f'{in_files[1]} is broken')
 
+    # неизвестно что будет, если версия Атмосферы не совпадёт
+    if not a_atmo_vernum == k_atmo_vernum:
+        sys.exit('Error: incompatible versions')
+
+    # новый файл должен быть меньше или равен старому, чтобы влезть
     if not a_boot_f_size > k_boot_f_size:
         k_data[0x1C4:0x1C8] = a_data[0x1C4:0x1C8]  # записываем новый размер файла
         k_data[0x4FC:0x500] = a_data[0x4FC:0x500]  # записываем новый размер файла
@@ -68,11 +78,12 @@ def main():
         # записываем данные нового файла по старому адресу
         k_data[k_boot_offset:k_boot_offset+a_boot_f_size] = a_data[a_boot_offset:a_boot_offset+a_boot_f_size]
 
+        # сохраняем изменённый файл на диск как package3 рядом со скриптом
         with open('package3', 'wb') as p:
             p.write(k_data)
-        print('Done: file saved as package3')
+        sys.exit('Done: file saved as package3')
     else:
-        print('Error: not enough free space')
+        sys.exit('Error: not enough free space')
 
 if __name__ == "__main__":
     main()
